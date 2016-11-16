57091a47046c41c707e5c69afcc8a5736bb32a18
adam <adam@pikicast.com>
2016-11-16 20:53:45 +0900

    day03 학생정보 시스템 초기단계 작업 진행중...

---
+++ src/test/java/sis/CourseSessionTest.java
@@ -0,0 +1,11 @@
+package sis;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by bollsal on 2016. 11. 16..
+ */
+public class CourseSessionTest {
+}
