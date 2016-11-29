1544f5a335e66c2050c592688c456e3a235e8fe4
adam <adam@pikicast.com>
2016-11-29 10:51:40 +0900

    day05 스터디진행

---
+++ src/main/java/elevator/Elevator.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by bollsal on 2016. 11. 28..
+ */
+public class Elevator {
+}
---
+++ src/test/java/elevator/ElevatorTest.java
@@ -0,0 +1,11 @@
+package elevator;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by bollsal on 2016. 11. 28..
+ */
+public class ElevatorTest {
+}
